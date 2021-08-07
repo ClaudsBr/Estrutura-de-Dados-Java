7b6de28222069f54c4352a90b01eb005b514c163
ClaudsBr <clauds.true@gmail.com>
2021-08-06 21:24:20 -0300

    Updating ListaDuplamenteEncadeada class

---
+++ src/main/java/one/digitalinnovation/nohduplo/ListaDuplamenteEncadeada.java
@@ -0,0 +1,103 @@
+package one.digitalinnovation.nohduplo;
+
+public class ListaDuplamenteEncadeada<T> {
+
+    private NohDuplo<T> primeiroNoh;
+    private NohDuplo<T> ultimoNoh;
+
+    private int tamanhoLista;
+
+    public ListaDuplamenteEncadeada() {
+        primeiroNoh = null;
+        ultimoNoh = null;
+        this.tamanhoLista = 0;
+    }
+
+    public int tamanho(){
+        return this.tamanhoLista;
+    }
+
+    private NohDuplo<T> getNoh(int index){
+        NohDuplo<T> nohAuxiliar = primeiroNoh;
+
+        for (int i= 0;(i < index) && (nohAuxiliar != null) ;i++){
+            nohAuxiliar = nohAuxiliar.getProximoNoh();
+        }
+        return nohAuxiliar;
+    }
+
+    public T get(int index){
+        return this.getNoh(index).getConteudo();
+    }
+
+    public void adicionar(T elemento){
+        NohDuplo<T> novoNoh = new NohDuplo<>(elemento);
+        novoNoh.setProximoNoh(null);
+        novoNoh.setNohAnterior(ultimoNoh);
+
+        if(primeiroNoh == null){
+            primeiroNoh = novoNoh;
+        }
+        if(ultimoNoh != null){
+            ultimoNoh.setProximoNoh(novoNoh);
+        }
+        ultimoNoh = novoNoh;
+        tamanhoLista++;
+    }
+
+    public void adicionarNaPosicao(int index, T elemento){
+        NohDuplo<T> nohAuxiliar = getNoh(index);
+        NohDuplo<T> novoNoh = new NohDuplo<>(elemento);
+        novoNoh.setProximoNoh(nohAuxiliar);
+
+        if(novoNoh.getProximoNoh() != null){
+            novoNoh.setNohAnterior(nohAuxiliar.getNohAnterior());
+            novoNoh.getProximoNoh().setNohAnterior(novoNoh);
+        }else{
+            novoNoh.setNohAnterior(ultimoNoh);
+            ultimoNoh = novoNoh;
+        }
+
+        if(index == 0){
+            primeiroNoh = novoNoh;
+        }else{
+            novoNoh.getNohAnterior().setProximoNoh(novoNoh);
+        }
+        tamanhoLista++;
+    }
+
+    public void remover(int index){
+
+        if(index == 0){
+            primeiroNoh = primeiroNoh.getProximoNoh();
+            if (primeiroNoh != null )  {
+                primeiroNoh.setNohAnterior(null);
+            }
+        }else{
+            NohDuplo<T> nohAuxiliar = getNoh(index);
+            nohAuxiliar.getNohAnterior().setProximoNoh(nohAuxiliar.getProximoNoh());
+
+            if(nohAuxiliar != ultimoNoh){
+                nohAuxiliar.getProximoNoh().setNohAnterior(nohAuxiliar.getNohAnterior());
+            }else{
+                ultimoNoh = nohAuxiliar;
+            }
+        }
+
+        this.tamanhoLista--;
+    }
+
+    @Override
+    public String toString() {
+        String retorno = "";
+
+        NohDuplo<T> nohAuxiliar = primeiroNoh;
+
+        for(int i =0 ; i < tamanho();i++){
+            retorno += "[Nó{conteúdo "+ nohAuxiliar.getConteudo() +"}] --> ";
+            nohAuxiliar = nohAuxiliar.getProximoNoh();
+        }
+        retorno += "null";
+        return retorno;
+    }
+}
---
+++ src/main/java/one/digitalinnovation/nohduplo/MainListaDuplamenteEncadeada.java
@@ -0,0 +1,31 @@
+package one.digitalinnovation.nohduplo;
+
+public class MainListaDuplamenteEncadeada {
+
+    public static void main(String[] args) {
+        ListaDuplamenteEncadeada<String> minhaLista = new ListaDuplamenteEncadeada<>();
+
+        minhaLista.adicionar("teste 1");
+        minhaLista.adicionar("teste 2");
+        minhaLista.adicionar("teste 3");
+        minhaLista.adicionar("teste 4");
+        minhaLista.adicionar("teste 5");
+        minhaLista.adicionar("teste 6");
+        minhaLista.adicionar("teste 7");
+
+        System.out.println(minhaLista);
+
+        minhaLista.remover(3);
+        System.out.println(minhaLista);
+
+        minhaLista.adicionarNaPosicao(3,"Novo teste");
+
+        System.out.println(minhaLista);
+
+        System.out.println(minhaLista.get(3));
+
+        System.out.println(minhaLista.tamanho());
+
+
+    }
+}
